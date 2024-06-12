210c61230a1b5540a92cfeeafe4a8f6b45ed1f48
Pcontreras98 <martin_contrerasn@hotmail.com>
2024-06-12 10:22:14 -0500

    addcategory2

--- src/helper/getGifs.js
+++ src/helper/getGifs.js
@@ -1,4 +1,4 @@
-export const getGifs = async( category,limit ) => {
+export const getGifs = async( category,limit=4 ) => {
 
     const url = `https://api.giphy.com/v1/gifs/search?api_key=WvyJfbVSQK9iCG80d6A5XFEXvU4dG9V2&q=${ category }&limit=${ limit }`;
     const resp = await fetch(url);
